f6ac5bbe14f05414c09d27118944418001ec1acc
Nicolas Vanz <nicavanz@hotmail.com>
2021-09-11 21:21:02 -0300

    AAAAAAAAA

--- main.py
+++ main.py
@@ -1,47 +1,39 @@
 import sys
 import time
-import multiprocessing
 import socket
 import fcntl, os
 import errno
 from time import sleep
 
-from socket import AF_INET, SOCK_STREAM
 from datetime import timedelta
-from multiprocessing import Pool
-from multiprocessing.connection import Listener, Client
 from concurrent.futures import ProcessPoolExecutor
 
-BASE_PORT = 8000
+BASE_PORT = 6080
 
 def node(index, process_number):
     in_address = ('localhost', BASE_PORT + index)
     out_address = ('localhost', BASE_PORT + ((index + 1) % process_number))
+
+    sock_in = socket.create_server(in_address, family=socket.AF_INET)
+    sock_in.listen(1)
     
-    inbox = socket.socket(AF_INET, SOCK_STREAM)
-    conn_out = socket.socket(AF_INET, SOCK_STREAM)
-
-    inbox.bind(in_address)
-    inbox.listen(1)
-    inbox.settimeout(5)
-
     if (index % 2 == 0):
-        conn_in, _ = inbox.accept()
-        conn_out.connect(out_address)
+        sock_out = socket.create_connection(out_address)
+        sock_in, addr = sock_in.accept()
     else:
-        conn_out.connect(out_address)
-        conn_in, _ = inbox.accept()
+        sock_in, addr = sock_in.accept()
+        sock_out = socket.create_connection(out_address)
 
-    fcntl.fcntl(conn_in, fcntl.F_SETFL, os.O_NONBLOCK)
+    fcntl.fcntl(sock_in, fcntl.F_SETFL, os.O_NONBLOCK)
 
     if (index != 0):
         data = f'{index}'
-        conn_out.send(data.encode())
+        sock_out.send(data.encode())
     
     ntrials = 5
     while (ntrials):
         try:
-            msg = conn_in.recv(1024)
+            msg = sock_in.recv(1024)
         except socket.error as e:
             err = e.args[0]
             if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
@@ -52,11 +44,9 @@ def node(index, process_number):
                 sys.exit(1)
         else:
             print(f'{index} received from {msg.decode()}')
+            break
     if (ntrials == 0):
-        print("nao recebeu")
-
-    conn_out.close()
-    conn_in.close()
+        print("not received")
 
     return index
 

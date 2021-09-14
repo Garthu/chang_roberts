b55bc60843f11ee3be328eb53c6bc6f5477b066a
Nicolas Vanz <nicavanz@hotmail.com>
2021-09-14 19:51:42 -0300

    safe zone

--- main.py
+++ main.py
@@ -3,14 +3,96 @@ import time
 import socket
 import fcntl, os
 import errno
-from time import sleep
+import random
 
+from time import sleep
+from message import Message
 from datetime import timedelta
 from multiprocessing import Semaphore, Barrier
 from concurrent.futures import ProcessPoolExecutor
 
+
 BASE_PORT = 6080
 
+def receive_data(sock_in, index):
+    ntrials = 5
+    while (ntrials):
+        msg = b""
+        try:
+            msg = sock_in.recv(1024)
+        except socket.error as e:
+            err = e.args[0]
+            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                sleep(1)
+                ntrials -= 1
+                continue
+            else:
+                sys.exit(1)
+        else:
+            break
+
+    data = Message()
+    data.decode(msg)
+
+    return data
+
+def election_message(data, sock_out, index):
+    if (data.sender == index):
+        data.set(code = "w", greatest_process = data.greatest_process)
+    elif (data.greatest_process < index):
+        data.set(greatest_process = index)
+    sock_out.send(data.encode())
+
+def winner_message(data, sock_out, index):
+    if (data.sender == index):
+        data = pick_message(index)
+    sock_out.send(data.encode())
+    
+
+def pick_message(index):
+    if (random.randint(1, 10) == 1):
+        msg = Message("e", sender = index)
+    else:
+        msg = Message("a", sender = index)
+    
+    return msg
+
+def alive_message(data, sock_out, index):
+    msg = pick_message(index)
+    sock_out.send(msg.encode())
+
+def node_behavior(sock_in, sock_out, index):
+    base_time = timedelta(seconds=process_number)
+    danger_time = timedelta(seconds=process_number * 3)
+    last_received_time = None
+    electing = False
+
+    if index == 0:
+        msg = Message("e", index, index)
+        sock_out.send(msg.encode())
+
+    while True:
+        data = receive_data(sock_in, index)
+        print(f'Index: {index}\ndata:{data.code}\n')
+
+        reset_time = last_received_time
+        last_received_time = time.monotonic()
+
+        if (data.code == "e"):
+            election_message(data, sock_out, index)
+        elif (data.code == "w"):
+            winner_message(data, sock_out, index)
+        elif (data.code == "a"):
+            alive_message(data, sock_out, index)
+        else:
+            last_received_time = reset_time
+            difference_time = timedelta(seconds=time.monotonic() - last_received_time)
+            
+            if (difference_time > base_time and difference_time < danger_time):
+                msg = Message("a")
+                sock_out.send(msg.encode(msg))
+            elif (difference_time >= danger_time):
+                pass # Caso processo anterior tenho morrido
 
 def node(index, process_number):
     in_address = ('localhost', BASE_PORT + index)
@@ -30,32 +112,11 @@ def node(index, process_number):
 
     fcntl.fcntl(sock_in, fcntl.F_SETFL, os.O_NONBLOCK)
 
-    #if (index != 0):
-    data = f'{index}'
-    sock_out.send(data.encode())
-    
-    ntrials = 5
-    while (ntrials):
-        try:
-            msg = sock_in.recv(1024)
-        except socket.error as e:
-            err = e.args[0]
-            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
-                sleep(1)
-                ntrials -= 1
-                continue
-            else:
-                sys.exit(1)
-        else:
-            print(f'{index} received from {msg.decode()}')
-            break
-    if (ntrials == 0):
-        print("not received")
-
-    return index
+    node_behavior(sock_in, sock_out, index)
 
 def main(argv):
     global barrier
+    global process_number
     start_time = time.monotonic()
     process_number = int(argv[1])
     barrier = Barrier(process_number)
--- message.py
+++ message.py
@@ -1,8 +1,26 @@
-from dataclasses import dataclass
-
-@dataclass
 class Message:
-    """Structure for organizing message data"""
-    code: str
-    greateste_process: int
-    sender: int
+    def __init__(self, code = "", greatest_process = -1, sender = -1):
+        self.code = code
+        self.greatest_process = greatest_process
+        self.sender = sender
+
+    def set(self, code = None, greatest_process = None, sender = None):
+        if (code):
+            self.code = code
+        if (greatest_process):
+            self.greatest_process = greatest_process
+        if (sender):
+            self.sender = sender
+
+
+    def encode(self):
+        return f'{self.code}#{self.greatest_process}#{self.sender}'.encode()
+
+    def decode(self, message):
+        message = message.decode()
+        if (message == ''):
+            return
+        code, greatest_process, sender = message.split("#")
+        self.code = code
+        self.greatest_process = int(greatest_process)
+        self.sender = int(sender)
